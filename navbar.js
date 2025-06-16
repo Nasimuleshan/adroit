e52cc96d45feddb41a73dfa5f2037ec464594797
Nasimuleshan <41768786+Nasimuleshan@users.noreply.github.com>
2025-06-16 14:21:52 +0600

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -31,6 +31,10 @@ document.addEventListener('DOMContentLoaded', function() {
         desktopNav.innerHTML = `
             <ul id="menu-1-031d597" class="elementor-nav-menu" data-smartmenus-id="17415734225721985">
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
+                    <a href="./index.html">
+<img width="150" height="47" src="./about_files/resc-logo.svg" class="attachment-full size-full wp-image-7776">								</a>
+                </li>
+				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
                     <a href="./index.html" class="elementor-item">Home</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
@@ -44,9 +48,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
                     <a href="./Projects.html" class="elementor-item">Projects</a>
-                </li>
-				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
-                    <a href="./our-team.html" class="elementor-item">Our Team</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
                     <!-- Empty item -->
@@ -77,9 +78,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
                     <a href="./Projects.html" class="elementor-item">Projects</a>
-                </li>
-				                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
-                    <a href="./our-team.html" class="elementor-item">Our Team</a>
                 </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
                     <!-- Empty item -->
